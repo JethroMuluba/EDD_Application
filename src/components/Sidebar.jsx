84f2e87a1674ff12a736bf7341ada4aa6ad6c1b4
JethroMuluba <jethro.muluba@gmail.com>
2024-04-18 21:49:33 +0100

    Fix : Gap of Sidebar Items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,8 +4,8 @@ import data from '../data/data.json';
 
 function Sidebar() {
   return (
-    <div className='w-1/5 flex flex-col gap-[220px] py-[24px] bg-cyan-950 '>
-      <nav className='flex flex-col  gap-[70px] '>
+    <div className='w-1/5 flex flex-col gap-[260px] py-[24px] bg-cyan-950 '>
+      <nav className='flex flex-col  gap-[30px] '>
 
         <img className='w-44 h-auto  px-[27px]' src={data.logo[0].darkMode} alt="Logo EDD App" />
 
