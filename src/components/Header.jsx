73fa2f1acd98a7abfb43b4f2fb5401bb4e014456
JethroMuluba <jethro.muluba@gmail.com>
2024-04-19 14:26:21 +0100

    Feat : Add Top Elements of Expensive Timeline.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,13 @@ import HeaderActions from './HeaderActions';
 function Header() {
   return (
     <header className='flex justify-between items-center shadow-md py-[17px] px-[24px]'>
+
         <h1 className='text-[26px] text-[#222834] '>
             Dashboard
         </h1>
 
         <HeaderActions />
 
-
-        
     </header>
   )
 }
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Header from '../components/Header';
 import Insights from '../components/Insights';
+import ExpensiveTimeLine from '../components/ExpensiveTimeLine';
 
 function Dashboard() {
   return (
-    <section className='bg-white w-4/5 text-black'>
+    <section className='bg-white w-4/5'>
       <Header/>
-      <main className='p-[24px] '>
+      <main className='flex flex-col gap-[24px] p-[24px] '>
         <Insights/>
+        <section>
+          <ExpensiveTimeLine/>
+        </section>
       </main>
 
     </section>
