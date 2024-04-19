fc506badeaa8442efa4141253a4c2960bdcc1f1f
JethroMuluba <jethro.muluba@gmail.com>
2024-04-19 12:53:09 +0100

    Feat : Add Last Insight

--- src/components/Insights.jsx
+++ src/components/Insights.jsx
@@ -4,11 +4,11 @@ import data from '../data/data.json';
 
 function Insights() {
   return (
-    <div className='flex justify-around'>
-        <div className='flex justify-between gap-[25px] py-[30px] pl-[27px] rounded-[10px] shadow-md py-[17px]'>
+    <div className='flex justify-between h-[7rem]'>
+        <div className='flex justify-between items-center gap-[25px] py-[30px] pl-[27px] rounded-[10px] shadow-md py-[17px]'>
 
-            <span className='bg-[#34B77B] rounded-full px-[20px] py-[14px] hover:bg-[#2FA46E]'>
-                <img className='pt-[7px] '  src={data.Insights[0].icone} alt="Calandar Icone" />
+            <span className='bg-[#34B77B] rounded-full px-[17px] py-[15px] px-[20px] py-[14px] hover:bg-[#2FA46E]'>
+                <img className='pt-[2px] '  src={data.Insights[0].icone} alt="Calandar Icone" />
             </span>
 
             <div className='flex flex-col mr-[25px] '>
@@ -25,9 +25,9 @@ function Insights() {
             </span>
         </div>
 
-        <div className='flex justify-between items-start gap-[20px] py-[30px] pl-[27px] rounded-[10px] shadow-md py-[17px]'>
+        <div className='flex justify-between items-center gap-[20px] py-[30px] pl-[27px] rounded-[10px] shadow-md py-[17px]'>
 
-            <span className='bg-[#F4BA40] rounded-full px-[16px] py-[15px]  hover:bg-[#DFAA3B]'>
+            <span className='bg-[#F4BA40] rounded-full px-[15px] py-[14px]  hover:bg-[#DFAA3B]'>
                 <img className='pt-[2px] '  src={data.Insights[1].icone} alt="Clock Icone" />
             </span>
 
@@ -41,7 +41,7 @@ function Insights() {
                 </h3>
             </div>
 
-            <button  className='pb-[35px] m-auto active:shadow-md active:rounded-full '>
+            <button  className='m-auto active:shadow-md active:rounded-full '>
                 <img src={data.Insights[1].moreIcone} alt="" />
             </button>
 
@@ -49,6 +49,26 @@ function Insights() {
                 <img src={data.Insights[1].border} alt="" />
             </span>
         </div>
+
+        <div className='flex justify-between items-center gap-[25px] py-[30px] pl-[27px] rounded-[10px] shadow-md py-[17px]'>
+
+            <span className='bg-[#DA5643] rounded-full px-[15px] py-[17px] hover:bg-[#B6493A]'>
+                <img className='pt-[px] '  src={data.Insights[2].icone} alt="Calandar Icone" />
+            </span>
+
+            <div className='flex flex-col mr-[25px] '>
+                <h2  className='text-[26px] text-[#222834] '>
+                    FC {data.Insights[2].mountant}
+                </h2>
+
+                <h3 className='text-base font-medium text-[#B2B3B5]'>
+                    {data.Insights[2].title}
+                </h3>
+            </div>
+                <span>
+                    <img src={data.Insights[2].border} alt="" />
+                </span>
+            </div>
 
 
     </div>
