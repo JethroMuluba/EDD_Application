5e629765c66dea343881eb7e076045b04d847f5b
JethroMuluba <jethro.muluba@gmail.com>
2024-04-22 13:19:54 +0100

    Feat : Navigate at Dashboard in Clicking on Logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,9 @@ function Sidebar() {
     <div className='w-1/5 flex flex-col gap-[260px] py-[24px] bg-cyan-950 '>
       <nav className='flex flex-col  gap-[30px] '>
 
-        <img className='w-44 h-auto  px-[27px]' src={data.logo[0].darkMode} alt="Logo EDD App" />
+        <Link to={'/'}>
+          <img className='w-44 h-auto  px-[27px]' src={data.logo[0].darkMode} alt="Logo EDD App" />
+        </Link>
 
         <ul className='flex flex-col gap-[px]'>
           <Link to={'/'} className='text-white'>
@@ -74,7 +76,7 @@ function Sidebar() {
       </nav>
 
       <div>
-        <span className='flex items-center  gap-[20px]  py-[10px] px-[27px] hover:bg-cyan-900 hover:text-[#F4CA8D]' onClick={() => handleClick('/Login')}>
+        <span className='flex items-center  gap-[20px]  py-[10px] px-[27px] cursor-pointer hover:bg-cyan-900 hover:text-[#F4CA8D]' onClick={() => handleClick('/Login')}>
             <span >
               <img src={data.icones[5].icone} alt="" />
             </span>
