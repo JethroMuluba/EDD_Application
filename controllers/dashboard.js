658c9067c4eb978438b1b4bb9d9bb157aa9ab7e9
JethroMuluba <jethro.muluba@gmail.com>
2024-05-03 10:05:25 +0100

    Feat : Create Route to Add Income and Expenses

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,48 +1,41 @@
 const prisma = require('../database/prima');
 
+const addIncomeAndExpense = async (req, res) => {
+    const { type, utilisateur, montant, categories } = req.body;
 
-const addIncoling = async (req, res) => {
-    const {utilisateur} = req.body
-    try {
-        createIncome = await prisma.revenuMensuelle.create({
-            data : {
-                    montant : req.body.montant,
-                    date : new Date(),
-                    utilisateur : { connect : { id : utilisateur }}
-            }
-        });
-        res.status(200).json(createIncome);
-        console.log('Amount is successfully Added');
-    } catch (error) {
-        res.status(500);
-        console.log(error);
+    if (type === 'revenu') {
+        try {
+            const createIncome = await prisma.revenuMensuelle.create({
+                data: {
+                    montant: montant,
+                    date: new Date(),
+                    utilisateur: { connect: { id: utilisateur }}
+                }
+            });
+            res.status(200).json({ message: 'Revenu ajouté avec succès', createIncome });
+        } catch (error) {
+            console.error("Erreur lors de l'ajout du revenu : ", error);
+            res.status(500).json({ message: "Erreur lors de l'ajout du revenu" });
+        }
+    } else if (type === 'depense') {
+        try {
+            const createExpense = await prisma.depenses.create({
+                data: {
+                    categories: categories,
+                    montant: montant,
+                    date: date,
+                    heure: date,
+                    utilisateur: { connect: { id: utilisateur }}
+                }
+            });
+            res.status(200).json({ message: 'Dépense ajoutée avec succès', createExpense });
+        } catch (error) {
+            console.error("Erreur lors de l'ajout de la dépense : ", error);
+            res.status(500).json({ message: "Erreur lors de l'ajout de la dépense" });
+        }
+    } else {
+        res.status(400).json({ message: 'Type de requête non spécifié' });
     }
 };
 
-const addExpenses = async (req, res) => {
-    const date = new Date();
-    const options = {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric'
-    };
-    const dateForma = date.toLocaleDateString('en-US', options).replace(/\//g, '-');
-    const heureForma = new Date().getHours() + ":" + new Date().getMinutes();
-
-    const {utilisateur, categories} = req.body;
-    try {
-        createExpenses = await prisma.depenses.create({
-            data : {
-                montant : req.body.montant,
-                date : dateForma,
-                heure : heureForma,
-                utilisateur : { connect : { id : utilisateur }},
-                categories : { connect : { id : categories }}
-            }
-        })
-    } catch (error) {
-        
-    }
-}
-
-module.exports = {addIncoling, addExpenses};
+module.exports = { addIncomeAndExpense };
