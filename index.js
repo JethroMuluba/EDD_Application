6aa1447f4cd8a3389a8ade88b457c5d8ced478ca
JethroMuluba <jethro.muluba@gmail.com>
2024-05-05 22:18:13 +0100

    Feat : Set Login Route

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express =  require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+require('./middlewares/passport_config');
+
 
 const dashboardRoute = require('./routes/dashboard');
 const userRegister = require('./routes/register');
+const loginRouter = require("./routes/login");
+const passport = require('passport');
 
 
 //Import Dotenv
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT;
+const jwtSecret = process.env.JWT_SECRET;
+
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(passport.initialize());
 
+
+//Set Login Route
+app.use('/login', loginRouter);
 //Set register Route
 app.use('/register', userRegister);
 
