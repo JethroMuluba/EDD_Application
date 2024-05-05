0df2ceddc021a370ad75fa78a7478305cb58d504
JethroMuluba <jethro.muluba@gmail.com>
2024-05-05 16:47:50 +0100

    Feat : Calculate all the expenses of the day and display only the total of these expenses

--- src/components/Insights.jsx
+++ src/components/Insights.jsx
@@ -1,42 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import data from '../data/data.json';
 import { useContextGlobal } from '../models/ContextGlobal';
 
 
 function Insights() {
-    const {getInsightData} = useContextGlobal();
+    const {getInsightData, getTableExpensiveData} = useContextGlobal();
     const insightDataIncome = getInsightData.montant;
 
-    // console.log(insightDataIncome);
-  return (
+    //Calculate all the expenses of the day and display only the total of these expenses
+    const [dailyExpenses, setDailyExpenses] = useState([]);
+    const [totalDailyExpenses, setTotalDailyExpenses] = useState();
+    
+    useEffect(() => {
+        const currentDate = new Date().toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '-');
+        // console.log("Current Date:", currentDate);
+    
+        const filteredExpenses = getTableExpensiveData.filter(expense => {
+            const expenseDate = new Date(expense.date).toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '-');
+            return expenseDate === currentDate;
+        });
+        // console.log("Filtered Expenses:", filteredExpenses);
+    
+        const totalExpenses = filteredExpenses.reduce((total, expense) => total + expense.montant, 0);
+        // console.log("Total Expenses:", totalExpenses);
+    
+        setDailyExpenses(filteredExpenses);
+        setTotalDailyExpenses(totalExpenses);
+    }, [getTableExpensiveData]);
+    return (
     
     <div className='grid grid-cols-1 md:grid-cols-3 gap-10  '>
-    {/* //     {insightData ? (insightData.map((data) => (
-    //                 <div key={data.id} className='flex justify-between items-center gap-[25px]  pl-[27px] rounded-[10px] shadow-md py-[17px]'>
-    //                 <span>
-    //                     <img className=''  src={data.icone} alt="Calandar Icone" />
-    //                 </span>
-        
-    //                 <div className='flex flex-col mr-[25px] '>
-    //                     <h2  className='text-[26px] text-[#222834] '>
-    //                         FC {data.amount}
-    //                     </h2>
-        
-    //                     <h3 className='text-base font-medium text-[#B2B3B5]'>
-    //                         {data.title}
-    //                     </h3>
-    //                 </div>
-
-    //                 <button  className='m-auto active:shadow-md active:rounded-full '>
-    //                     <img src={data.moreIcone} alt="" />
-    //                 </button>
-    //                 <span>
-    //                     <img src={data.border} alt="" />
-    //                 </span>
-    //             </div>
-    //     ))) : 'Pas de revenu'} */}
-
-    
 
         <div className='flex justify-between items-center gap-[20px]  pl-[27px] rounded-[10px] shadow-md py-[17px]'>
 
@@ -73,7 +66,7 @@ function Insights() {
 
             <div className='flex flex-col '>
                 <h2  className='text-[26px] text-[#222834] '>
-                    FC {data.insights[1].amount}
+                    FC {totalDailyExpenses.toLocaleString('fr-FR', { maximumFractionDigits: 0 })}
                 </h2>
 
                 <h3 className='text-base font-medium text-[#B2B3B5]'>
