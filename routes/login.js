db524c5a24adf1b08d7e9dc4e5a5e0b0ff1a6752
JethroMuluba <jethro.muluba@gmail.com>
2024-05-05 21:52:16 +0100

    Feat : Create Login Route

---
+++ routes/login.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const loginRouter = express.Router();
+const prisma = require('../database/prima');
+
+
+loginRouter.post('/', async (req, res, next) => {
+    passport.authenticate('local', { session : false}, (err, user, info) => {
+        if (err) {
+            return next(err);
+        };
+
+        if (!user) {
+            return res.status(401).json({ message: info.message });
+        };
+
+        req.login(user, {session : false }, async (err) => {
+            if (err) {
+                return next(err);
+            };
+
+            const token = jwt.sign({ id: user.id }, jwtSecret);
+
+            return res.json({token});
+        }); 
+    }) (req, res, next);
+});
+
+module.exports = loginRouter;
